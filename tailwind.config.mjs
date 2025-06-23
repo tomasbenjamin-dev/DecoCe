22c530621045d62c9e18bcef9ede745349e34983
Tomas Ponce <work.tomasps@gmail.com>
2025-06-22 23:45:26 -0300

    add pre-shop page

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        rosa: {
+          pastel: '#FCCDDE',
+          nube: '#FFE6EF',
+          cuarzo: '#E6A9C4',
+          te: '#EEC2D0',
+          vino: '#D889A3',
+          petalo: '#FDEDF2',
+        },
+      },
+    },
+  },
+}
